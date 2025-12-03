b77804825a770035d34a6f61b98197a931ca521d
Artur Ostrovskiy <artur_ostrovskiy@mail.ru>
2025-12-03 18:43:30 +0300

    commit

--- LESSON13/script.js
+++ LESSON13/script.js
@@ -1,23 +1,22 @@
-const list = document.querySelector('.list')
+const list = document.querySelector('.list') //присваивает переменной list html элемент с классом list, а именно <ol class="list">…</ol>
 
-list.addEventListener('click', function (event) {
-	const targetElement = event.target
-	if (targetElement.classList.contains('item')) {
-		targetElement.classList.toggle('done')
+list.addEventListener('click', function (event) { //по клику передается параметр event в функцию
+	const targetElement = event.target //присваиваем targetElement значение event, а .target указывает на конкретный элемент
+	if (targetElement.classList.contains('item')) { //проверка на наличие класса item в targetElement
+		targetElement.classList.toggle('done') //classList содержит список всех классов targetElement
 	}
 })
 
+const form = document.querySelector('.form') //присваивает переменной form html элемент с классом form
+form.addEventListener('submit', function (event) { //по клику на элемент submit передается параметр event в функцию
+	event.preventDefault() //.preventDefault() это метод который прерывает стандартное поведение браузера связанное с этим событием
+	const input = document.querySelector('.input') //присваивает переменной input html элемент с классом input
+	const text = input.value //присваивает переменной text значение(.value) переданное в input
+	input.value = '' //присваивает полю ввода input значение '' (пустой строки)
 
-const form = document.querySelector('.form')
-form.addEventListener('submit', function (event) {
-	event.preventDefault()
-	const input = document.querySelector('.input')
-	const text = input.value
-	input.value = ''
 
-
-	const newItem = document.createElement('li')
-	newItem.classList.add('item')
-	newItem.textContent = text
-	list.append(newItem)
+	const newItem = document.createElement('li') //присваиваем переменной newItem созданный новый элемент 'li'
+	newItem.classList.add('item') //добавляем нашей новой li класс item
+	newItem.textContent = text //добавляем нашей новой li текст из input.value (<li>text</li>)
+	list.append(newItem) //добавляем в элемент с классом list наш newItem
 })
